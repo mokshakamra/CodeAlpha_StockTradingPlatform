7fc544812271af3d08a91f281c63eb6972029328
mokshakamra <mokshakamra@gmail.com>
2026-06-25 00:54:12 +0530

    Add files via upload

---
+++ src/model/PortfolioItem.java
@@ -0,0 +1,30 @@
+package model;
+
+public class PortfolioItem {
+
+    private Stock stock;
+    private int quantity;
+    private double purchasePrice;
+
+    public PortfolioItem(Stock stock, int quantity, double purchasePrice) {
+        this.stock = stock;
+        this.quantity = quantity;
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ src/service/TradingService.java
@@ -0,0 +1,352 @@
+package service;
+
+import model.PortfolioItem;
+import model.Stock;
+import model.Transaction;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class TradingService {
+
+    private ArrayList<Stock> marketStocks;
+    private ArrayList<PortfolioItem> portfolio;
+    private ArrayList<Transaction> transactions;
+
+    private double walletBalance;
+
+    private final String FILE_NAME = "portfolio.txt";
+
+    public TradingService() {
+
+        marketStocks = new ArrayList<>();
+        portfolio = new ArrayList<>();
+        transactions = new ArrayList<>();
+
+        walletBalance = 100000;
+
+        loadMarketStocks();
+
+        loadPortfolio();
+    }
+
+    private void loadMarketStocks() {
+
+        marketStocks.add(new Stock("AAPL", "Apple", 190));
+        marketStocks.add(new Stock("GOOGL", "Google", 250));
+        marketStocks.add(new Stock("TSLA", "Tesla", 300));
+        marketStocks.add(new Stock("MSFT", "Microsoft", 220));
+    }
+
+    public void viewMarket() {
+
+        System.out.println("\n===== MARKET DATA =====");
+
+        for (Stock stock : marketStocks) {
+            System.out.println(stock);
+        }
+    }
+
+    public void buyStock(String symbol, int quantity) {
+
+        Stock stock = findStock(symbol);
+
+        if (stock == null) {
+            System.out.println("Stock not found!");
+            return;
+        }
+
+        double totalCost = stock.getPrice() * quantity;
+
+        if (totalCost > walletBalance) {
+            System.out.println("Insufficient wallet balance!");
+            return;
+        }
+
+        walletBalance -= totalCost;
+
+        PortfolioItem existingItem = findPortfolioItem(symbol);
+
+        if (existingItem != null) {
+
+            existingItem.setQuantity(
+                    existingItem.getQuantity() + quantity
+            );
+
+        } else {
+
+            portfolio.add(
+                    new PortfolioItem(
+                            stock,
+                            quantity,
+                            stock.getPrice()
+                    )
+            );
+        }
+
+        transactions.add(
+                new Transaction(
+                        "BUY",
+                        symbol,
+                        quantity,
+                        totalCost
+                )
+        );
+
+        savePortfolio();
+        System.out.println("Stock purchased successfully!");
+    }
+
+    public void sellStock(String symbol, int quantity) {
+
+        PortfolioItem item = findPortfolioItem(symbol);
+
+        if (item == null) {
+            System.out.println("You do not own this stock.");
+            return;
+        }
+
+        if (quantity > item.getQuantity()) {
+            System.out.println("Not enough quantity.");
+            return;
+        }
+
+        double sellAmount =
+                item.getStock().getPrice() * quantity;
+
+        walletBalance += sellAmount;
+
+        item.setQuantity(
+                item.getQuantity() - quantity
+        );
+
+        if (item.getQuantity() == 0) {
+            portfolio.remove(item);
+        }
+
+        transactions.add(
+                new Transaction(
+                        "SELL",
+                        symbol,
+                        quantity,
+                        sellAmount
+                )
+        );
+
+        savePortfolio();
+
+        System.out.println("Stock sold successfully!");
+    }
+
+    public void viewPortfolio() {
+
+        System.out.println("\n===== PORTFOLIO =====");
+
+        if (portfolio.isEmpty()) {
+            System.out.println("Portfolio is empty.");
+            return;
+        }
+
+        for (PortfolioItem item : portfolio) {
+
+            double currentValue =
+                    item.getQuantity()
+                            * item.getStock().getPrice();
+
+            System.out.println(
+                    item.getStock().getSymbol()
+                            + " | Qty: "
+                            + item.getQuantity()
+                            + " | Current Value: ₹"
+                            + currentValue
+            );
+        }
+
+        System.out.println(
+                "\nTotal Portfolio Value: ₹"
+                        + calculatePortfolioValue()
+        );
+    }
+
+    public void showTransactions() {
+
+        System.out.println("\n===== TRANSACTIONS =====");
+
+        if (transactions.isEmpty()) {
+            System.out.println("No transactions available.");
+            return;
+        }
+
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction);
+        }
+    }
+
+    public double calculatePortfolioValue() {
+
+        double total = 0;
+
+        for (PortfolioItem item : portfolio) {
+
+            total += item.getQuantity()
+                    * item.getStock().getPrice();
+        }
+
+        return total;
+    }
+
+    public void calculateProfitLoss() {
+
+        double investedAmount = 0;
+        double currentValue = 0;
+
+        for (PortfolioItem item : portfolio) {
+
+            investedAmount +=
+                    item.getPurchasePrice()
+                            * item.getQuantity();
+
+            currentValue +=
+                    item.getStock().getPrice()
+                            * item.getQuantity();
+        }
+
+        double profitLoss =
+                currentValue - investedAmount;
+
+        System.out.println(
+                "\nInvested Amount: ₹" + investedAmount
+        );
+
+        System.out.println(
+                "Current Value: ₹" + currentValue
+        );
+
+        System.out.println(
+                "Profit/Loss: ₹" + profitLoss
+        );
+    }
+
+    public void showWalletBalance() {
+
+        System.out.println(
+                "\nWallet Balance: ₹"
+                        + walletBalance
+        );
+    }
+
+    private Stock findStock(String symbol) {
+
+        for (Stock stock : marketStocks) {
+
+            if (stock.getSymbol()
+                    .equalsIgnoreCase(symbol)) {
+
+                return stock;
+            }
+        }
+
+        return null;
+    }
+
+    private PortfolioItem findPortfolioItem(String symbol) {
+
+        for (PortfolioItem item : portfolio) {
+
+            if (item.getStock()
+                    .getSymbol()
+                    .equalsIgnoreCase(symbol)) {
+
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+    private void savePortfolio() {
+
+        try (BufferedWriter writer =
+                     new BufferedWriter(
+                             new FileWriter(FILE_NAME))) {
+
+            writer.write(String.valueOf(walletBalance));
+            writer.newLine();
+
+            for (PortfolioItem item : portfolio) {
+
+                writer.write(
+                        item.getStock().getSymbol()
+                                + ","
+                                + item.getQuantity()
+                                + ","
+                                + item.getPurchasePrice()
+                );
+
+                writer.newLine();
+                System.out.println("Portfolio saved to: " + new File(FILE_NAME).getAbsolutePath());
+            }
+
+        } catch (IOException e) {
+
+            System.out.println(
+                    "Error saving portfolio: "
+                            + e.getMessage()
+            );
+        }
+    }
+
+    private void loadPortfolio() {
+
+        File file = new File(FILE_NAME);
+
+        if (!file.exists()) {
+            return;
+        }
+
+        try (BufferedReader reader =
+                     new BufferedReader(
+                             new FileReader(FILE_NAME))) {
+
+            String line = reader.readLine();
+
+            if (line != null) {
+                walletBalance =
+                        Double.parseDouble(line);
+            }
+
+            while ((line = reader.readLine()) != null) {
+
+                System.out.println("Read line: " + line);
+
+                String[] data = line.split(",");
+
+                String symbol = data[0];
+                int quantity = Integer.parseInt(data[1]);
+                double purchasePrice = Double.parseDouble(data[2]);
+
+                Stock stock = findStock(symbol);
+
+                if (stock != null) {
+
+                    portfolio.add(
+                            new PortfolioItem(
+                                    stock,
+                                    quantity,
+                                    purchasePrice
+                            )
+                    );
+
+                    System.out.println("Loaded: " + symbol);
+                }
+            }
+
+        } catch (IOException e) {
+
+            System.out.println(
+                    "Error loading portfolio: "
+                            + e.getMessage()
+            );
+        }
+    }
+}
